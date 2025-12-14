23817ab7f79d94b3c2b1a2cad777f72b4aa1d45b
anthony-m-schauer <anthony.m.schauer@gmail.com>
2025-12-14 15:36:34 -0600

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -7,6 +7,14 @@ def run_kmeans(df, k):
     print("K-Means complete. Labels shape:", labels.shape)
     return labels, model
 
+def run_hierarchical(df, k, metric="euclidean"):
+    print(f"Running Hierarchical clustering with k={k} using metric={metric}...")
+    model = AgglomerativeClustering(n_clusters=k, metric=metric, linkage="average")
+    labels = model.fit_predict(df)
+    print("Hierarchical clustering complete. Labels shape:", labels.shape)
+    return labels, model
+
+'''
 def run_hierarchical(df):
     print(f"Running Hierarchical clustering with k={k}...")
     model = AgglomerativeClustering(n_clusters=k)
@@ -14,7 +22,6 @@ def run_hierarchical(df):
     print("Hierarchical clustering complete. Labels shape:", labels.shape)
     return labels, model
 
-'''
 def run_dbscan(df, eps=0.5, min_samples=5):
     print(f"Running DBSCAN with eps={eps}, min_samples={min_samples}...")
     model = DBSCAN(eps=eps, min_samples=min_samples)
